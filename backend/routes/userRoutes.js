497c3a2c54616b0d60e4815a95314bea6eff9d70
MOHAMMED ASIF K V <mohammedasifkv@gmail.com>
2024-12-10 11:12:30 +0530

    Basic backend completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,9 +7,11 @@ import { authUser,
     updateUserProfile
 } from '../controllers/userController.js';// we want to give the extension
                                          // bacuse type is module(will throw error module not found)
+import { protect } from '../middleware/authMiddleware.js';
+
 router.post('/',registerUser)
 router.post('/auth',authUser)
 router.post('/logout',logoutUser)
-router.route('/profile').get(getUserProfile).put(updateUserProfile)
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
 
 export default router;
